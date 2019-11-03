b5daab76b61f659d81670754faeea0f78d3ee9b2
Anastasiia Pavlenko <anastasia2ustimets@gmail.com>
2019-11-03 15:20:44 +0200

    edited disabled

--- src/Controls/Controls.jsx
+++ src/Controls/Controls.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Controls.module.css';
 
-const Controls = ({ onIncrement, onDecrement, disabled }) => (
+const Controls = ({ onIncrement, onDecrement, index }) => (
     < section className={styles.controls}>
-        <button disabled={disabled} type="button" className={styles.button} onClick={onDecrement}>Назад</button>
+        <button disabled={(index === 0 || index === index.length - 1) ? true : false} type="button" className={styles.button} onClick={onDecrement}>Назад</button>
         <button type="button" className={styles.button} onClick={onIncrement}>Вперед</button>
     </section >
 );
@@ -12,7 +12,7 @@ const Controls = ({ onIncrement, onDecrement, disabled }) => (
 Controls.propTypes = {
     onIncrement: PropTypes.func.isRequired,
     onDecrement: PropTypes.func.isRequired,
-    disabled: PropTypes.bool.isRequired,
+    index: PropTypes.number.isRequired,
 };
 
 export default Controls;
